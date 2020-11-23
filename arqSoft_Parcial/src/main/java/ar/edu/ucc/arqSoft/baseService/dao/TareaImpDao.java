15cb1dbf19c3e42d77534f99a3f1009a5daad4c6
CindyOrtega416 <cindyortega416@gmail.com>
2020-11-23 00:33:35 -0300

    agregué cambios hechos de la otra branch

--- arqSoft_Parcial/src/main/java/ar/edu/ucc/arqSoft/baseService/dao/TareaImpDao.java
+++ arqSoft_Parcial/src/main/java/ar/edu/ucc/arqSoft/baseService/dao/TareaImpDao.java
@@ -1,6 +1,7 @@
 package ar.edu.ucc.arqSoft.baseService.dao;
 
 import java.util.List;
+import java.util.Date;
 
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
@@ -8,12 +9,16 @@ import javax.persistence.criteria.Root;
 
 import org.springframework.stereotype.Repository;
 
+import ar.edu.ucc.arqSoft.baseService.model.Estado;
+import ar.edu.ucc.arqSoft.baseService.model.Proyecto;
 import ar.edu.ucc.arqSoft.baseService.model.Tarea;
+import ar.edu.ucc.arqSoft.baseService.model.Usuario;
 import ar.edu.ucc.arqSoft.common.dao.GenericDaoImp;
 
 @Repository
 public class TareaImpDao extends GenericDaoImp<Tarea, Long> implements TareaDao {
 
+	@Override
 	public List<Tarea> FindByName (String nombre){
 
 		CriteriaBuilder builder = em.getCriteriaBuilder();
@@ -23,4 +28,38 @@ public class TareaImpDao extends GenericDaoImp<Tarea, Long> implements TareaDao 
         criteria.select(entity).where(builder.equal(entity.get("nombre"+" "+"apellido"), nombre));
         return em.createQuery(criteria).getResultList();
 	}
+	
+	@Override
+	public List<Tarea> findByTarea(String nombre_tarea){
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Tarea> criteria = builder.createQuery(Tarea.class);
+		Root<Tarea> entity = criteria.from(Tarea.class);
+		
+		criteria.select(entity).where(builder.equal(entity.get("nombre_tarea"), nombre_tarea));
+		return em.createQuery(criteria).getResultList();
+	}
+	
+	@Override
+	public List<Tarea> findByProyecto(Proyecto proyecto){
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Tarea> criteria = builder.createQuery(Tarea.class);
+		Root<Tarea> entity = criteria.from(Tarea.class);
+		
+		criteria.select(entity).where(builder.equal(entity.get("proyecto"), proyecto));
+		return em.createQuery(criteria).getResultList();
+	}
+	
+	@Override
+	public Tarea createTarea(String nombre_tarea, String descripcion, Usuario usuario, Proyecto proyecto, Estado estado, Date fecha_inicio,  Date ultima_actualizacion) {
+		
+		CriteriaBuilder builder = em.getCriteriaBuilder();
+		CriteriaQuery<Tarea> criteria = builder.createQuery(Tarea.class);
+		Root<Tarea> entity = criteria.from(Tarea.class);
+		
+	}		
+	
+	
+	
 }
